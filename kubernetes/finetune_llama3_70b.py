b665745b327b8b31fc7dcccf788e58dbf02c8287
wenxindongwork <wenxindong@google.com>
2024-07-21 14:12:44 -0700

    use cache = false

--- kubernetes/finetune_llama3_70b.py
+++ kubernetes/finetune_llama3_70b.py
@@ -33,7 +33,7 @@ data = dataset.map(preprocess_function, remove_columns=list(dataset.features))
 
 print("----1----")
 
-model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=True)
+model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=False)
 
 print("----2----")
 
